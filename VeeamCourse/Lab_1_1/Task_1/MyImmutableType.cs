8d9abb7c78b2756a32a99e048dbec6b765b2a13c
Dell-Laptop <Dell-Laptop@DESKTOP-NVTEF0S>
2021-10-10 19:29:10 +0300

    Добавьте файлы проекта.

---
+++ VeeamCourse/Lab_1_1/Task_1/MyImmutableType.cs
@@ -0,0 +1,15 @@
+﻿namespace VeeamCourse.Lab_1_1
+{
+    class MyImmutableType
+    {
+
+        public int SomeField { get; private set; }
+
+        public MyImmutableType SetSomeField(int newValue) => new() { SomeField = newValue };
+
+        public override string ToString()
+        {
+            return this.SomeField.ToString();
+        }
+    }
+}
---
+++ VeeamCourse/Lab_1_1/Task_2/Structs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace VeeamCourse.Lab_1_1.Task_2
+{
+    class Structs
+    {
+        public struct Base
+        {
+            public void SayHi()
+            {
+                Console.WriteLine("Hi from the Base struct!");
+            }
+
+            public void SayBye()
+            {
+                Console.WriteLine("Bye from the Base struct!");
+            }
+        }
+
+        public struct Derived
+        {
+            private Base baseStruct;
+
+            public Derived(Base baseStruct)
+            {
+                this.baseStruct = baseStruct;
+            }
+
+            // Inheritance simulation
+            public void SayHi()
+            {
+                baseStruct.SayHi();
+            }
+
+            // Ovverriding simulation
+            public void SayBye()
+            {
+                Console.WriteLine("Bye from the Derived struct!");
+            }
+            
+        }
+    }
+}
